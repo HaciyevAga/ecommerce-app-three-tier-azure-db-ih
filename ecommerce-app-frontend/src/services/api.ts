06e0952a6616d2d18eae3ef435b082635fc3988c
Yunis Quliyev <155447445+Ichihiroy@users.noreply.github.com>
2026-04-01 10:09:52 +0400

    Update api.ts

--- ecommerce-app-frontend/src/services/api.ts
+++ ecommerce-app-frontend/src/services/api.ts
@@ -12,7 +12,7 @@ import {
   CreateOrderData 
 } from '../types';
 
-const API_BASE_URL = "https://app-backend-group2-aec2d0c7htfccrdp.westus2-01.azurewebsites.net/api"
+const API_BASE_URL = "http://20.42.155.71/api"
 
 // Create axios instance
 const api = axios.create({
